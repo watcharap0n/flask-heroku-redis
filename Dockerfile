41b04b4d4e74461a1807a7104d7e559433d83631
watcharap0n <wera.watcharapon@gmail.com>
2021-11-20 16:28:58 +0700

    push docker

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.8
+
+WORKDIR /app
+ENV FLASK_APP=app.py
+ENV FLASK_RUN_HOST=0.0.0.0
+RUN apt-get update ##[edited]
+RUN apt-get install 'ffmpeg'\
+    'libsm6'\
+    'libxext6' -y
+#RUN apk add --no-cache gcc musl-dev linux-headers
+
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+ENV PORT 8080
+
+WORKDIR /app
+COPY . .
+CMD exec gunicorn --bind :$PORT --workers 3 app:app
--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-from flask import request, Flask, jsonify
+import time
+
+from redis import Redis, exceptions
+from flask import Flask
 
 app = Flask(__name__)
+cache = Redis(host='redis', port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 
 @app.route('/')
-def index():
-    return '<h2> Hello Flask </h2>'
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
